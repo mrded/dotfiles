95f5614665e8b7dd58a6c18fb846943f70c8bd49
Dmitry Demenchuk <dmitry@demenchuk.me>
2022-08-30 19:53:03 +0100

    install dependancies for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,13 +2,28 @@ return function(use)
   use {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.0',
+    run = 'brew install fd ripgrep',
     requires = {
       'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
     },
     config = function() 
       vim.cmd [[
         nnoremap <C-p> :Telescope git_files<CR>
       ]]
+
+      require('telescope').setup {
+        extensions = {
+          fzf = {
+            fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = true,  -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+          }
+        }
+      }
+
+      require('telescope').load_extension('fzf')
     end
   }
 end
