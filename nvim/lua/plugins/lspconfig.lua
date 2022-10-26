e352727ff4be8c813dde08bd7c70d783d2f133f4
Dmitry Demenchuk <dmitry@demenchuk.me>
2022-10-26 21:26:40 +0100

    clean up nvim-lspconfig config

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -9,7 +9,7 @@
 -- Autocompletion settings of "nvim-cmp" are defined in plugins/nvim-cmp.lua
 
 
-local do_config = function() 
+local do_config = function()
   vim.diagnostic.config({
     virtual_text = false,
     -- signs = true,
@@ -26,13 +26,14 @@ end
 
 local do_keymap = function(bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
-  local opts = { noremap=true, silent=true }
+  local opts = { noremap = true, silent = true }
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   buf_set_keymap('n', '<C-r>', '<cmd>LspRestart<CR>', opts)
@@ -41,66 +42,75 @@ local do_keymap = function(bufnr)
   buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
---  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
---  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
---  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
---  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
---  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  --  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  --  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  --  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  --  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  --  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_set_keymap('n', '<CR>', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
---  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  --  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
   buf_set_keymap('n', 'ge', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', 'gE', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
---  buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
---  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  --  buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  --  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   do_config()
   do_keymap(bufnr)
 end
 
-get_handlers = function()
+local get_handlers = function()
   -- Hover doc popup
   local pop_opts = { border = "rounded", max_width = 80 }
 
   return {
     ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, pop_opts),
     ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, pop_opts)
-  } 
+  }
+end
+
+local handlers = get_handlers()
+
+local config = function()
+  local lsp_status_ok, lspconfig = pcall(require, 'lspconfig')
+  if not lsp_status_ok then
+    return
+  end
+
+  local cmp_status_ok = pcall(require, 'cmp_nvim_lsp')
+  if not cmp_status_ok then
+    return
+  end
+
+  -- Use a loop to conveniently call 'setup' on multiple servers and
+  -- map buffer local keybindings when the language server attaches
+  local servers = {
+    'tsserver',
+    'gopls',
+    'yamlls',
+    'sumneko_lua',
+    'eslint',
+    -- 'volar'
+  }
+
+  for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+      handlers = handlers,
+      on_attach = on_attach,
+      flags = { debounce_text_changes = 150, }
+    }
+  end
 end
 
 return function(use)
   use {
     'neovim/nvim-lspconfig',
     tag = 'v0.1.3',
-    run = 'brew install gopls yaml-language-server lua-language-server && npm install -g yaml-language-server typescript typescript-language-server',
-    config = function() 
-      local lsp_status_ok, lspconfig = pcall(require, 'lspconfig')
-      if not lsp_status_ok then
-        return
-      end
-
-      local cmp_status_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
-      if not cmp_status_ok then
-        return
-      end
-
-      -- Use a loop to conveniently call 'setup' on multiple servers and
-      -- map buffer local keybindings when the language server attaches
-      local servers = { 'tsserver', 'gopls', 'yamlls', 'sumneko_lua' }
-
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup {
-          handlers = get_handlers(),
-          on_attach = on_attach,
-          flags = {
-            debounce_text_changes = 150,
-            }
-          }
-      end
-    end
+    run = 'brew install gopls yaml-language-server lua-language-server && npm install -g yaml-language-server typescript typescript-language-server vscode-langservers-extracted @volar/vue-language-server',
+    config = config
   }
 end
