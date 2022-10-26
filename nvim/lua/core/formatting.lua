a22898212774e039c7446c0f5df10c519a5c2dab
Dmitry Demenchuk <dmitry@demenchuk.me>
2022-10-26 22:49:04 +0100

    make a separate script for formatting rules

--- nvim/lua/core/formatting.lua
+++ nvim/lua/core/formatting.lua
@@ -2,9 +2,18 @@ local lsp_formatting = function()
     vim.lsp.buf.format({ async = false })
 end
 
+local js_formatting = function()
+    -- see: http://neovim.io/doc/user/lsp.html#vim.lsp.buf.format()
+    vim.lsp.buf.format {
+    async = false
+    -- Never request typescript-language-server for formatting
+    -- filter = function(client) return client.name ~= "tsserver" end
+}
+end
+
 vim.api.nvim_create_autocmd(
     "BufWritePre",
-    { pattern = { "*.js", "*.ts", "*.tsx" }, callback = lsp_formatting }
+    { pattern = { "*.js", "*.ts", "*.tsx" }, callback = js_formatting }
 )
 
 vim.api.nvim_create_autocmd(
