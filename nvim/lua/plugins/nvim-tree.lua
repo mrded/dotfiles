c1a331865eb2f6684cc8b99302b8af23c4f45874
Dmitry Demenchuk <dmitry@demenchuk.me>
2022-11-28 12:26:50 +0000

    update nvim-tree settings

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -2,32 +2,45 @@ return function(use)
   use {
     'nvim-tree/nvim-tree.lua',
     tag = 'nightly',
-    config = function() 
-      -- disable netrw 
+    config = function()
+      -- disable netrw
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
 
       require("nvim-tree").setup({
-          view = {
-            adaptive_size = true,
-            mappings = {
-              list = {
-                { key = "s", action = "vsplit" },
-                { key = "t", action = "tabnew" },
-              },
+        view = {
+          adaptive_size = true,
+          mappings = {
+            list = {
+              { key = "s", action = "vsplit" },
+              { key = "t", action = "tabnew" },
             },
           },
-          renderer = {
-            icons = { 
-              show = {
-                file = false,
-                folder = false,
-                folder_arrow = false,
-                git = false,
-              },
+        },
+        renderer = {
+          icons = {
+            show = {
+              file = false,
+              folder = false,
+              folder_arrow = false,
+              git = false,
             },
           },
-        })
+        },
+        filters = {
+          dotfiles = false,
+          custom = {
+            -- "^\\.env",
+          },
+          exclude = {},
+        },
+        git = {
+          enable = true,
+          ignore = true,
+          show_on_dirs = true,
+          timeout = 400,
+        },
+      })
 
       -- keybindings
       local opts = { noremap = true, silent = true }
