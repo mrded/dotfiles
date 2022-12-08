a0413decb1df57e3342155fc9d7a9c661368d5d0
Dmitry Demenchuk <dmitry@demenchuk.me>
2022-12-08 12:25:51 +0000

    use Eslint to format JS

--- nvim/lua/core/formatting.lua
+++ nvim/lua/core/formatting.lua
@@ -2,19 +2,7 @@ local lsp_formatting = function()
     vim.lsp.buf.format({ async = false })
 end
 
-local js_formatting = function()
-    -- see: http://neovim.io/doc/user/lsp.html#vim.lsp.buf.format()
-    vim.lsp.buf.format {
-    async = false
-    -- Never request typescript-language-server for formatting
-    -- filter = function(client) return client.name ~= "tsserver" end
-}
-end
-
-vim.api.nvim_create_autocmd(
-    "BufWritePre",
-    { pattern = { "*.js", "*.ts", "*.tsx" }, callback = js_formatting }
-)
+vim.cmd('autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js,*.vue EslintFixAll')
 
 vim.api.nvim_create_autocmd(
     "BufWritePre",
