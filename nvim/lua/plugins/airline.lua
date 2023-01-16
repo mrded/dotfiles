eba3974acada2c1d061713252d433c1fe455db60
Dmitry Demenchuk <dmitry@demenchuk.me>
2023-01-16 22:16:06 +0000

    Make tabs to show only numbers

--- nvim/lua/plugins/airline.lua
+++ nvim/lua/plugins/airline.lua
@@ -1,33 +1,32 @@
 return {
   "vim-airline/vim-airline",
-  tag = 'v0.11',
+  -- tag = 'v0.11',
   config = function()
     vim.cmd [[
-        let g:airline#extensions#tabline#enabled = 1
-        let g:airline#extensions#tabline#formatter = 'unique_tail'
-        let g:airline#extensions#tabline#fnamemod = ':t'
-        let g:airline#extensions#tabline#show_tab_count = 0
-        let g:airline#extensions#tabline#show_tab_type = 0
-        let g:airline#extensions#tabline#show_close_button = 0
-        let g:airline#extensions#tabline#show_tab_nr = 0
-        let g:airline#extensions#tabline#show_splits = 0
-        let g:airline#extensions#tabline#show_buffers = 0
-        let g:airline#extensions#tabline#tab_min_count = 2
-      ]]
+      let g:airline#extensions#tabline#enabled = 1
+      let g:airline#extensions#tabline#formatter = 'tabnr'
+      let g:airline#extensions#tabline#show_tab_count = 0
+      let g:airline#extensions#tabline#show_tab_type = 0
+      let g:airline#extensions#tabline#show_close_button = 0
+      let g:airline#extensions#tabline#show_tab_nr = 0
+      let g:airline#extensions#tabline#show_splits = 0
+      let g:airline#extensions#tabline#show_buffers = 0
+      let g:airline#extensions#tabline#tab_min_count = 2
+    ]]
 
     vim.cmd [[
-        let g:airline_theme='gruvbox'
-        let g:airline#extensions#clock#auto = 0
-        function! AirlineInit()
-        let g:airline_section_a = '' " mode, crypt, paste, spell, iminsert
-        let g:airline_section_b = '' " hunks, branch
-        " let g:airline_section_c = '' " bufferline or filename, readonly
-        " let g:airline_section_x = '' " tagbar, filetype, virtualenv
-        let g:airline_section_y = '' " fileencoding, fileformat
-        " let g:airline_section_z = '' " percentage, line number, column number
-        let g:airline_section_z = airline#section#create(['codeowners'])
-        endfunction
-        autocmd User AirlineAfterInit call AirlineInit()
-      ]]
+      let g:airline_theme='gruvbox'
+      let g:airline#extensions#clock#auto = 0
+      function! AirlineInit()
+      let g:airline_section_a = '' " mode, crypt, paste, spell, iminsert
+      let g:airline_section_b = '' " hunks, branch
+      " let g:airline_section_c = '' " bufferline or filename, readonly
+      " let g:airline_section_x = '' " tagbar, filetype, virtualenv
+      let g:airline_section_y = '' " fileencoding, fileformat
+      " let g:airline_section_z = '' " percentage, line number, column number
+      let g:airline_section_z = airline#section#create(['codeowners'])
+      endfunction
+      autocmd User AirlineAfterInit call AirlineInit()
+    ]]
   end
 }
