29dfaa1923945bcc5c49217f561744ce352aae31
Dmitry Demenchuk <dmitry@demenchuk.me>
2023-02-28 22:37:27 +0000

    update vgit

--- nvim/lua/plugins/vgit.lua
+++ nvim/lua/plugins/vgit.lua
@@ -1,20 +1,20 @@
 return {
   'tanvirtin/vgit.nvim',
-  tag = 'v0.1.2',
+  tag = 'v0.2.1',
   requires = {
     'nvim-lua/plenary.nvim'
   },
   config = function()
-    require('vgit').setup({
+    local vgit = require('vgit')
+
+    vgit.setup({
       settings = {
-        live_blame = {
-          enabled = false,
-        }
+        live_blame = { enabled = false },
+        live_gutter = { enabled = false },
       }
     })
 
-    vim.api.nvim_create_user_command('Ghistory', require("vgit").buffer_history_preview, { nargs = 0 })
-    vim.api.nvim_create_user_command('Gblame', require("vgit").buffer_blame_preview, { nargs = 0 })
-    vim.api.nvim_create_user_command('GblameGutter', require("vgit").buffer_gutter_blame_preview, { nargs = 0 })
+    vim.api.nvim_create_user_command('Ghistory', vgit.buffer_history_preview, { nargs = 0 })
+    vim.api.nvim_create_user_command('Gblame', vgit.buffer_gutter_blame_preview, { nargs = 0 })
   end
 }
