324e466a46ee2924a3de7b6be6568011fc73ea49
Dmitry Demenchuk <dmitry@demenchuk.me>
2023-02-28 22:49:39 +0000

    open nvim-tree on startup

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -47,5 +47,23 @@ return {
 
     vim.api.nvim_set_keymap('n', '<C-n>', "<cmd>NvimTreeToggle<CR>", opts)
     vim.api.nvim_set_keymap('n', '<C-b>', "<cmd>NvimTreeFindFile<CR>", opts)
+
+    -- open nvim-tree on startup
+    vim.api.nvim_create_autocmd({ "VimEnter" }, {
+      callback = function(data)
+        -- buffer is a directory
+        local directory = vim.fn.isdirectory(data.file) == 1
+
+        if not directory then
+          return
+        end
+
+        -- change to the directory
+        vim.cmd.cd(data.file)
+
+        -- open the tree
+        require("nvim-tree.api").tree.open()
+      end
+    })
   end
 }
