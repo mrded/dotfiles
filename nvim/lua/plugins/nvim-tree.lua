9b6a825edaf497ef00aef78839f8660b13b1f825
Dmitry Demenchuk <dmitry@demenchuk.me>
2023-03-19 22:15:01 +0000

    change a way to open current file in nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -46,7 +46,7 @@ return {
     local opts = { noremap = true, silent = true }
 
     vim.api.nvim_set_keymap('n', '<C-n>', "<cmd>NvimTreeToggle<CR>", opts)
-    vim.api.nvim_set_keymap('n', '<C-b>', "<cmd>NvimTreeFindFile<CR>", opts)
+    vim.api.nvim_set_keymap('n', '<C-b>', "<cmd>NvimTreeFindFileToggle<CR>", opts)
 
     -- open nvim-tree on startup
     vim.api.nvim_create_autocmd({ "VimEnter" }, {
