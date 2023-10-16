26ff11ceaa02335ac9cadeb220cd7563a37265c8
Dmitry Demenchuk <dmitry@demench.uk>
2023-10-16 21:57:57 +0100

    add an alias for Vs -> vs

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -1,5 +1,6 @@
 -- One of the most annoying things about vim was that I frequently typed :W instead of :w.
 vim.cmd('command! W w')
+vim.cmd('command! V v')
 
 vim.cmd('command! TestRun vsplit term://npx jest % --watch --runInBand --no-cache --coverage=false')
 vim.api.nvim_create_user_command('Rename', function() vim.lsp.buf.rename() end, { nargs = 0 })
