086e2a2537577fba8e6e509dc10a3b53a6672697
Dmitry Demenchuk <dmitry@demench.uk>
2023-12-27 15:39:36 +0000

    run tests in a floating window

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -2,11 +2,35 @@
 vim.cmd('command! W w')
 vim.cmd('command! V v')
 
-vim.cmd('command! TestRun vsplit term://npx jest % --watch --runInBand --no-cache --coverage=false')
+vim.cmd('command! TestRunWatch vsplit term://npx jest % --watch --runInBand --no-cache --coverage=false')
 vim.cmd('command! JestRun vsplit term://npx jest % --watch --runInBand --no-cache --coverage=false')
 vim.cmd('command! VitestRun vsplit term://npx vitest run % --watch --coverage=false')
 vim.api.nvim_create_user_command('Rename', function() vim.lsp.buf.rename() end, { nargs = 0 })
-vim.api.nvim_create_user_command('Format', function() vim.lsp.buf.format() end, { nargs = 0 })
+-- vim.api.nvim_create_user_command('Format', function() vim.lsp.buf.format() end, { nargs = 0 })
+
+vim.api.nvim_create_user_command('TestRun', function()
+  local buf = vim.api.nvim_create_buf(false, true)
+  local width = vim.api.nvim_get_option("columns")
+  local height = vim.api.nvim_get_option("lines")
+
+  -- calculate the center position
+  local win_height = math.ceil(height * 0.9 - 4)
+  local win_width = math.ceil(width * 0.9)
+  local row = math.ceil((height - win_height) / 2 - 1)
+  local col = math.ceil((width - win_width) / 2)
+
+  vim.api.nvim_open_win(buf, true, {
+    style = "minimal",
+    relative = "editor",
+    width = win_width,
+    height = win_height,
+    row = row,
+    col = col
+  })
+
+  local file_path = vim.fn.expand("%")
+  vim.fn.termopen("npx jest " .. file_path .. " --runInBand --no-cache --coverage=false")
+end, {})
 
 vim.api.nvim_set_keymap('n', '<C-r>', '', {
   noremap = true,
