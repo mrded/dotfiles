7bb913684e8ddc32874169758854d0c563f8fbe8
Dmitry Demenchuk <dmitry@demench.uk>
2024-03-07 14:32:47 +0000

    prettify

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -6,31 +6,31 @@
 -- See: https://neovim.io/doc/user/vim_diff.html
 -- [2] Defaults - *nvim-defaults*
 
-local g = vim.g       -- Global variables
-local opt = vim.opt   -- Set options (global/buffer/windows-scoped)
+local g = vim.g     -- Global variables
+local opt = vim.opt -- Set options (global/buffer/windows-scoped)
 
 -----------------------------------------------------------
 -- General
 -----------------------------------------------------------
-opt.mouse = 'a'                       -- Enable mouse support
+opt.mouse = 'a'      -- Enable mouse support
 -- opt.clipboard = 'unnamedplus'         -- Copy/paste to system clipboard
-opt.swapfile = false                  -- Don't use swapfile
+opt.swapfile = false -- Don't use swapfile
 -- opt.completeopt = 'menuone,noinsert,noselect'  -- Autocomplete options
 opt.spelllang = 'en_gb'
 
 -----------------------------------------------------------
 -- Neovim UI
 -----------------------------------------------------------
-opt.number = true           -- Show line number
-opt.showmatch = false       -- Highlight matching parenthesis
-opt.foldmethod = 'syntax'   -- Enable folding (default 'foldmarker')
-opt.splitright = true       -- Vertical split to the right
-opt.splitbelow = true       -- Horizontal split to the bottom
-opt.ignorecase = false      -- Ignore case letters when search
-opt.smartcase = true        -- Ignore lowercase for the whole pattern
-opt.linebreak = true        -- Wrap on word boundary
-opt.termguicolors = true    -- Enable 24-bit RGB colors
-opt.laststatus=3            -- Set global statusline
+opt.number = true         -- Show line number
+opt.showmatch = false     -- Highlight matching parenthesis
+opt.foldmethod = 'syntax' -- Enable folding (default 'foldmarker')
+opt.splitright = true     -- Vertical split to the right
+opt.splitbelow = true     -- Horizontal split to the bottom
+opt.ignorecase = false    -- Ignore case letters when search
+opt.smartcase = true      -- Ignore lowercase for the whole pattern
+opt.linebreak = true      -- Wrap on word boundary
+opt.termguicolors = true  -- Enable 24-bit RGB colors
+opt.laststatus = 3        -- Set global statusline
 
 vim.cmd [[
   set winbar=%=%m\ %f
@@ -38,24 +38,24 @@ vim.cmd [[
 
 vim.cmd [[
   set nofoldenable
-]] -- Prevent automatic folding 
+]] -- Prevent automatic folding
 
 -----------------------------------------------------------
 -- Tabs, indent
 -----------------------------------------------------------
-opt.expandtab = true        -- Use spaces instead of tabs
-opt.shiftwidth = 2          -- Shift 4 spaces when tab
-opt.tabstop = 2             -- 1 tab == 4 spaces
-opt.smartindent = true      -- Autoindent new lines
+opt.expandtab = true   -- Use spaces instead of tabs
+opt.shiftwidth = 2     -- Shift 4 spaces when tab
+opt.tabstop = 2        -- 1 tab == 4 spaces
+opt.smartindent = true -- Autoindent new lines
 
 -----------------------------------------------------------
 -- Memory, CPU
 -----------------------------------------------------------
-opt.hidden = true           -- Enable background buffers
-opt.history = 100           -- Remember N lines in history
-opt.lazyredraw = true       -- Faster scrolling
-opt.synmaxcol = 240         -- Max column for syntax highlight
-opt.updatetime = 700        -- ms to wait for trigger an event
+opt.hidden = true     -- Enable background buffers
+opt.history = 100     -- Remember N lines in history
+opt.lazyredraw = true -- Faster scrolling
+opt.synmaxcol = 240   -- Max column for syntax highlight
+opt.updatetime = 700  -- ms to wait for trigger an event
 
 -----------------------------------------------------------
 -- Startup
