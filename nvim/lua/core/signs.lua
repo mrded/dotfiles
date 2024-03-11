728ca04babcb01ca88451dd732376261a8b06294
Dmitry Demenchuk <dmitry@demench.uk>
2024-03-11 20:21:09 +0000

    add signs / bookmarks

---
+++ nvim/lua/core/signs.lua
@@ -0,0 +1,11 @@
+vim.cmd([[sign define heartSign text=♥ texthl=Hint]])
+
+vim.api.nvim_create_user_command('Bookmark', function()
+  local current_line = vim.fn.line('.')
+  local cmd = string.format("sign place 1 line=%d name=heartSign", current_line)
+  vim.cmd(cmd)
+end, {})
+
+vim.api.nvim_create_user_command('UnBookmark', function()
+  vim.cmd("sign unplace")
+end, {})
