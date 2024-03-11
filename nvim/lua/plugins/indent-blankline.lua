8f20f2a6506f6c00b16fa69828f08262093ff8c8
Dmitry Demenchuk <dmitry@demench.uk>
2024-03-11 19:25:09 +0000

    update indent-blankline plugin

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,7 +1,7 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  tag = 'v2.20.2',
+  tag = 'v3.5.3',
   config = function()
-    require("indent_blankline").setup {}
+    require("ibl").setup()
   end
 }
