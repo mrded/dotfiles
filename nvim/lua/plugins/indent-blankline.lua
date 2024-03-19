1402c7fb452bd7e0ae11cfe3429448a8039a5710
Dmitry Demenchuk <dmitry@demench.uk>
2024-03-19 17:19:42 +0000

    disable scope highlighting

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -2,6 +2,8 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   tag = 'v3.5.3',
   config = function()
-    require("ibl").setup()
+    require("ibl").setup {
+      scope = { enabled = false },
+    }
   end
 }
