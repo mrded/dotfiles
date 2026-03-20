bad8bac827789bb19638383bc2d3068a08d8c65c
Dmitry Demenchuk <dmitry@demench.uk>
2026-03-20 10:16:15 +0000

    configure vim-illuminate with proper delays
    vim-illuminate triggering LSP on every cursor move with no throttling.

--- nvim/lua/plugins/illuminate.lua
+++ nvim/lua/plugins/illuminate.lua
@@ -1,5 +1,20 @@
 -- Highlights words under the cursor
--- TODO: can probably done natively.
 return {
   'RRethy/vim-illuminate',
+  config = function()
+    require('illuminate').configure({
+      -- Delay in milliseconds before highlighting
+      delay = 300,
+      -- Don't use LSP as a provider to avoid performance issues
+      providers = {
+        'treesitter',
+        'regex',
+      },
+      -- Large file handling
+      large_file_cutoff = 2000,
+      large_file_overrides = {
+        providers = { 'treesitter' },
+      },
+    })
+  end,
 }
