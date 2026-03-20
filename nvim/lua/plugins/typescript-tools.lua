cb657076cb514a376dfb55801aa8f539fc0ab467
Dmitry Demenchuk <dmitry@demench.uk>
2026-03-20 11:12:47 +0000

    filter out unwanted code actions (e.g., "Move to file", "Move to a new file")

--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -4,8 +4,25 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
   config = function()
     local lsp_config = require('core.lsp')
+
+    -- Filter out unwanted code actions (e.g., "Move to file", "Move to a new file")
+    local original_ui_select = vim.ui.select
+    vim.ui.select = function(items, opts, on_choice)
+      if opts.prompt and opts.prompt:match("Code action") then
+        items = vim.tbl_filter(function(item)
+          local title = type(item) == "table" and item.action and item.action.title or ""
+          return not title:match("^Move to")
+        end, items)
+      end
+
+      return original_ui_select(items, opts, on_choice)
+    end
+
     require('typescript-tools').setup({
       on_attach = lsp_config.on_attach,
+      settings = {
+        expose_as_code_action = "all",
+      },
     })
   end,
 }
